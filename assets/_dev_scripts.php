726eef8b8f19396e4e213d0d61bea2acd302d75d
bambattajb <joe@white-fire.co.uk>
2016-06-15 14:04:02 +0100

    Fix relative script and style path

--- assets/_dev_scripts.php
+++ assets/_dev_scripts.php
@@ -1,7 +1,7 @@
 <?php
 function _dev_scripts() {
     $output  = '<!-- For Dev -->' . "\n";
-    $output .='<script data-main="'.get_template_directory_uri().'/js/main" src="'.get_template_directory_uri() . '/functions/WF/assets/js/requirejs/require.js"></script>' . "\n";
+    $output .='<script data-main="'.get_template_directory_uri().'/js/main" src="'.get_template_directory_uri() . '/WF-Bootstrap/assets/js/requirejs/require.js"></script>' . "\n";
     $output .='<!-- END For Dev -->'  . "\n";
 
     echo $output;
--- assets/_dev_styles.php
+++ assets/_dev_styles.php
@@ -6,7 +6,7 @@ function _dev_styles() {
     $output     = '<!-- For Dev -->' . "\n";
     $output     .= '<link rel="stylesheet/less" type="text/css" href="'.get_template_directory_uri().'/css/main.less">'  . "\n";
     $output     .= '<script type="text/javascript">less = { env: "development" };</script>' . "\n";
-    $output     .='<script type="text/javascript" src="'.get_template_directory_uri() .  '/functions/WF/assets/js/less.js/dist/less.min.js"></script>'  . "\n";
+    $output     .='<script type="text/javascript" src="'.get_template_directory_uri() .  '/WF-Bootstrap/assets/js/less.js/dist/less.min.js"></script>'  . "\n";
     $output     .='<script>localStorage.clear(); </script>'  . "\n";
     $output     .= '<script type="text/javascript">less.modifyVars({ "@fa-font-path" : "\'../'.ltrim($wf_config['FONTAWESOME_PATH'], '/').'/fonts\'" });</script>' . "\n";
 
